595becdc55bfd490d7f48ebab6dbc7ed27a432f1
MadFloot <madflootyt@gmail.com>
2025-09-30 18:17:59 -0600

    inicion de sesion yregistro de usuarios

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace KN_ProyectoWeb.Controllers
 {
@@ -12,6 +8,9 @@ namespace KN_ProyectoWeb.Controllers
         {
             return View();
         }
-
+        public ActionResult Registro()
+        {
+            return View();
+        }
     }
 }
