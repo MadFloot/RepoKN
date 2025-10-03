a49d1fb514fd4d001e0f6655ce218db286d72259
MadFloot <madflootyt@gmail.com>
2025-10-02 21:49:24 -0600

    fin de semana 3

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,28 +1,59 @@
-﻿using System.Web.Mvc;
+﻿using KN_ProyectoWeb.Models;
+using System.Web.Mvc;
 
 namespace KN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
+        #region Iniciar Sesion
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            return RedirectToAction("Principal", "Home");
+        }
+
+        #endregion
+
+        #region Registro
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            return View();
+        }
+        #endregion
 
+        #region Recuperar Acesso
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario)
+        {
+            return View();
+        }
+        #endregion
 
 
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
         }
+
   
     }
 }
